561287d20df24cd8aa7e307097fe7b61b1a4a5f6
Kartikeya Shorya <kartikeyashorya@gmail.com>
2023-11-18 00:12:29 +0530

    Implemented audio recorder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import vmsg from 'vmsg'
+
+const recorder = new vmsg.Recorder({
+  wasmURL: 'https://unpkg.com/vmsg@0.3.0/vmsg.wasm'
+})
+
+class App extends React.Component{
+
+  state = {
+    isLoading:false,
+    isRecording:false,
+    recording:null
+  }
+
+  record = async () => {
+    this.setState({isLoading:true})
+
+    if(this.state.isRecording){
+      const blob = await recorder.stopRecording()
+      this.setState({
+        isLoading: false,
+        isRecording: false,
+        recording:URL.createObjectURL(blob)
+      })
+    }
+
+    else {
+      try {
+        await recorder.initAudio()
+        await recorder.initWorker()
+        recorder.startRecording()
+        this.setState({isLoading:false, isRecording: true})
+      } catch (error) {
+        alert(error.message)
+        this.setState({isLoading: false})
+      }
+    }
+  }
+
+  render() {
+    const {isLoading, isRecording, recording} = this.state;
+
+    return(
+      <React.Fragment>
+        <button onClick={this.record} disabled={isLoading}>
+          {isRecording ? "Stop Recording" : "Start Recording"}
+        </button>
+        <br/>
+        {
+          recording != null && isRecording == false
+          ? <audio src={recording} controls/>
+          : null
+        }
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
